6f9705a499adc3433f0daad4383a148364d534d9
Ignacio Egozcue <IE304632@fi365.ort.edu.uy>
2025-04-02 21:31:56 -0300

    Agregar archivos de proyecto.

---
+++ AgenciaEnvios.LogicaNegocio/InterfacesRepositorios/IRepositorioAgencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaEnvios.LogicaNegocio.InterfacesRepositorios
+{
+    public interface IRepositorioAgencia
+    {
+    }
+}
